da6547f2807765160c5c2bf3187b6fe4a643c4ee
lebeddan <lebeddan@fel.cvut.cz>
2021-05-20 00:28:48 +0200

    add a powerup

---
+++ src/main/java/com/gameengine/game/gameobjects/PowerUps/FastShoot.java
@@ -0,0 +1,31 @@
+package com.gameengine.game.gameobjects.PowerUps;
+
+import com.gameengine.engine.GameContainer;
+import com.gameengine.engine.Renderer;
+import com.gameengine.game.GameManager;
+import com.gameengine.game.gameobjects.GameObject;
+import javafx.geometry.Point2D;
+
+public class FastShoot extends GameObject {
+    private String pathToImage = "";
+
+    @Override
+    public void update(GameContainer gc, GameManager gm, float dt) {
+
+    }
+
+    @Override
+    public void render(GameContainer gc, Renderer r) {
+
+    }
+
+    @Override
+    public void hit(GameObject obj) {
+
+    }
+
+    @Override
+    public Point2D getCenter() {
+        return null;
+    }
+}
